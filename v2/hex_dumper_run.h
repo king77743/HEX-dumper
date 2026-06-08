36a8c7e663f40f103db84f1edd068a6f8ce9935d
Root💻 <artemmatvienko154@gmail.com>
2026-06-08 14:06:00 +0500

    Update hex_dumper_run.h

--- v2/hex_dumper_run.h
+++ v2/hex_dumper_run.h
@@ -1,64 +1,143 @@
+#ifndef LOGICA_H
+#define LOGICA_H
 #include <stdio.h>
-#include <stdlib.h>
 #include "clear_bufer.h"
-#include "magic_bytes.h"
-#include "hex_dumper_run.h"
+void run_analyze(FILE *file){
+    fseek(file,0,SEEK_END);
+    long size_file=ftell(file);
+    fseek(file,0,SEEK_SET);
 
-int main() {
-    system(""); 
+    int mode;
+    int bytes;
+    printf("[ 1: Full analyze (%ld bytes)  |  2: Custom byte limit ]\n",size_file);
+    
     while (1){
-        int command;
-        printf("\033[1;37m[ 1: Analyze  |  2: Clear  |  3: Exit ]\033[0m\n");
-        printf("\033[1;37mSelect command >\033[0m ");
-        scanf("%d",&command);
+        printf("Select analyze mode > ");
+        scanf("%d",&mode);
         clear_buff();
-        if (command==2){
-            system("cls");
-            continue;
-        }
-        else if (command==3){
+        if (mode==1){
+            bytes=(int)size_file;
             break;
         }
-        else if (command<=0||command>3){
-            printf("ERROR: Command is invalid\n");
-            continue;
-        }
-        while (1) { 
-            FILE* file = NULL;
-            char path[260];        
-            while (1) {
-                printf("\033[1;32m[+]\033[0m Path to file: ");
-                if (fgets(path, sizeof(path), stdin) == NULL) {
-                    return 1;
+        else if(mode==2){
+            printf("File size: %ld bytes\n",size_file);
+            while (1){
+                printf("Enter bytes to read: ");
+                if (scanf("%d",&bytes)!=1){
+                    printf("ERROR: Invalid number!\n");
+                    clear_buff();
+                    continue;
                 }
-                for (int i = 0; i < sizeof(path); i++) {
-                    if (path[i] == '\n') {
-                        path[i] = '\0';
-                        break;
-                    }
-                    if (path[i] == '\0') {
-                        break;
-                    }
+                clear_buff();
+                if (bytes<0){
+                    printf("bytes cannot be negative\n");
+                    continue;
                 }
-                file = fopen(path, "rb");
-                if (file == NULL) {
-                    printf("\033[1;31m[-] ERROR: File not found!\033[0m\n");
+                if (bytes>size_file){
+                    printf("you cannot read %d bytes. File only has %ld bytes\n",bytes,size_file);
                     continue;
                 }
                 break;
             }
-            type_file(file);
-            run_analyze(file);
-            fclose(file);
-            printf("\n\033[1;33m[!]\033[0m Do you want to analyze another file? (y/n): ");
-            int response = getchar();  
-            clear_buff();
-
-            if (response != 'y' && response != 'Y') {
-                break; 
-            }  
-            printf("\n"); 
+            break;
+        }
+        else{
+            printf ("ERROR: Invalid mode!\n");
+            continue;
         }
     }
-    return 0;
+
+    int count = 0, address = 0;
+    int ch;
+    char buff[16];
+    
+    while(1){
+        int total=0;
+    
+        while (total<bytes && (ch = fgetc(file)) != EOF) {
+            
+            if (count == 0) {
+                printf("\033[1;36m%08X\033[0m  ", address);
+            }
+            printf("%02X ", ch);
+            if (ch >= 32 && ch <= 126) {
+                buff[count] = (char)ch;
+            } else {
+                buff[count] = '.';
+            }
+            count++;
+            total++;
+            if (count == 16) {
+                printf(" | \033[1;32m");
+                for (int i = 0; i < 16; i++) {
+                    printf("%c", buff[i]);
+                }
+                printf("\033[0m\n");
+                address += 16;
+                count = 0;
+            }
+        }
+        
+        if (ch==EOF){
+            if (count > 0) {
+                for (int i = count; i < 16; i++) {
+                    printf("   ");
+                }
+                printf(" | \033[1;32m");
+                for (int i = 0; i < count; i++) {
+                    printf("%c", buff[i]);
+                }
+                printf("\033[0m\n");
+            }
+            printf("End of file!\n");
+            break;
+        }
+        if (count > 0) {
+                for (int i = count; i < 16; i++) {
+                    printf("   ");
+                }
+                printf(" | \033[1;32m");
+                for (int i = 0; i < count; i++) {
+                    printf("%c", buff[i]);
+                }
+                printf("\033[0m\n");
+            }
+
+        printf("\nContinue Reading? (1: Yes / 2: NO): ");
+        int choice;
+        scanf("%d",&choice);
+        clear_buff();
+        
+        if (choice==1) {
+            long cont=size_file-ftell(file);
+            if (cont<=0){
+                printf("No more bytes to read!\n");
+                if (count > 0) {
+                    for (int i = count; i < 16; i++) printf("   ");
+                    printf(" | \033[1;32m");
+                    for (int i = 0; i < count; i++) printf("%c", buff[i]);
+                    printf("\033[0m\n");
+                }
+                break;
+            }
+            while(1){
+                printf("Enter next bytes (max %ld): ",cont);
+                scanf("%d",&bytes);
+                clear_buff();
+                if (bytes<0 || bytes>cont){
+                    printf("Invalid bytes!\n");
+                    continue;
+                }
+                break;
+            }
+            
+            printf("\033[2K\033[A\033[2K\033[A\033[2K\033[A");
+            fflush(stdout);
+        }
+        else{
+            
+            break;
+        }
+    }   
 }
+#endif
